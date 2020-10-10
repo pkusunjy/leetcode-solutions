9481d0ce3a419bc6eeb6567a5f55805f6b27d915
pkusunjy <767978963@qq.com>
2020-10-10 18:59:13 +0800

    0141

--- src/0141_Linked_List_Cycle.cpp
+++ src/0141_Linked_List_Cycle.cpp
@@ -6,11 +6,7 @@ public:
     bool hasCycle(ListNode* head) {
         ListNode* fast = head;
         ListNode* slow = head;
-<<<<<<< HEAD
         while(fast != nullptr && fast->next != nullptr) {
-=======
-        while(fast != NULL && fast->next != NULL) {
->>>>>>> 00592616b9e2d87de4170d9e937d21ff0e3c22d8
             slow = slow->next;
             fast = fast->next->next;
             if (slow == fast) return true;
