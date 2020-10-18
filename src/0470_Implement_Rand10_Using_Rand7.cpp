566f43dc87d08b589c03b5cb8438d541c9b104eb
pkusunjy <767978963@qq.com>
2020-10-18 17:36:55 +0800

    0470

---
+++ src/0470_Implement_Rand10_Using_Rand7.cpp
@@ -0,0 +1,19 @@
+#include "global.hpp"
+using namespace std;
+
+class Solution {
+public:
+    int rand10() {
+        int row, col, idx;
+        do {
+            row = rand7();
+            col = rand7();
+            idx = col + (row - 1) * 7;
+        } while (idx > 40);
+        return 1 + (idx - 1) % 10;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
