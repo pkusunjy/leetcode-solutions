cedd913bd3c436a8f11e94b86d4d75f8e2e380c6
pkusunjy <767978963@qq.com>
2020-10-18 10:59:31 +0800

    1071

---
+++ src/1071_Greatest_Common_Divisor_of_Strings.cpp
@@ -0,0 +1,19 @@
+#include "global.hpp"
+using namespace std;
+
+class Solution {
+public:
+    int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1)  return "";
+        return str1.substr(0, gcd(str1.size(), str2.size()));
+    }
+};
+
+int main(int argc, char* argv[]) {
+    Solution s;
+    cout << s.gcdOfStrings("ABCABC", "ABC");
+    return 0;
+}
